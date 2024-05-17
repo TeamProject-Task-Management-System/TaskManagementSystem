e2dd9ac655caa6bb41e49729058f78f065095db0
DimitarKomitski <mitkokomitski@icloud.com>
2024-05-17 15:55:35 +0300

    BoardImpl

--- src/com/company/oop/teamProject/models/BoardImpl.java
+++ src/com/company/oop/teamProject/models/BoardImpl.java
@@ -1,4 +1,37 @@
 package com.company.oop.teamProject.models;
 
-public class BoardImpl {
+import com.company.oop.teamProject.models.contracts.Board;
+import com.company.oop.teamProject.models.contracts.EventLog;
+import com.company.oop.teamProject.models.contracts.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardImpl extends TeamImpl implements Board {
+    private List<Task> tasksList = new ArrayList<>();
+    private List<EventLog> activityHistory = new ArrayList<>();
+
+    public BoardImpl(String name) {
+        super(name);
+    }
+
+    @Override
+    public List<Task> getTasks() {
+        return new ArrayList<>(tasksList);
+    }
+
+    @Override
+    public List<EventLog> getActivityHistory() {
+        return new ArrayList<>(activityHistory);
+    }
+
+    @Override
+    public void addTask(Task task) {
+        tasksList.add(task);
+    }
+
+    @Override
+    public void addActivity(EventLog eventLog) {
+        activityHistory.add(eventLog);
+    }
 }
