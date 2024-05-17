de28c881ba4245345b64c03d33f12933aa241a61
David.Grigorov <david.grigorov02@gmail.com>
2024-05-17 15:58:48 +0300

    EventLOgimpl

--- src/com/company/oop/teamProject/models/EventLogImpl.java
+++ src/com/company/oop/teamProject/models/EventLogImpl.java
@@ -1,4 +1,34 @@
 package com.company.oop.teamProject.models;
 
-public class EventLogImpl {
+import com.company.oop.teamProject.models.contracts.EventLog;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class EventLogImpl implements EventLog {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss");
+    private String description;
+    private LocalDate timeStamp;
+
+    public EventLogImpl(String description){
+        setDescription(description);
+        this.timeStamp = LocalDate.now();
+    }
+
+    @Override
+    public String getDescription(){
+        return String.format("[%s] %s", timeStamp.format(formatter), description);
+    }
+
+    public LocalDate getTimeStamp(){
+        return timeStamp;
+    }
+
+    public void setDescription(String description){
+        if ( description.isEmpty()){
+            throw new IllegalArgumentException("Description cannot be empty");
+        }
+        this.description = description;
+        this.timeStamp = LocalDate.now();
+    }
 }
