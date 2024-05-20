c2983b12b9dd52826aaf8a8ba46d35622b7f1be4
DimitarKomitski <mitkokomitski@icloud.com>
2024-05-20 17:44:37 +0300

    BaseCommand add

---
+++ src/com/company/oop/teamProject/command/contracts/BaseCommand.java
@@ -0,0 +1,24 @@
+package com.company.oop.teamProject.command.contracts;
+
+import com.company.oop.teamProject.core.contracts.TaskManagementRepository;
+
+import java.util.List;
+
+public abstract class BaseCommand implements Command {
+
+    private final TaskManagementRepository taskManagementRepository;
+
+    protected BaseCommand(TaskManagementRepository taskManagementRepository) {
+        this.taskManagementRepository = taskManagementRepository;
+    }
+
+    protected TaskManagementRepository getTaskManagementRepository() {
+        return taskManagementRepository;
+    }
+
+    public String execute(List<String> parameters) {
+        return executeCommand(parameters);
+    }
+
+    protected abstract String executeCommand(List<String> parameters);
+}
