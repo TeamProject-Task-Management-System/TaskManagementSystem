b852c0c0aaa5ad88df9bee5ceedcdfdfd3ae8fed
DimitarKomitski <mitkokomitski@icloud.com>
2024-05-21 20:57:36 +0300

    small change in create commands

--- src/com/company/oop/teamProject/command/ShowAllTeamMembersCommand.java
+++ src/com/company/oop/teamProject/command/ShowAllTeamMembersCommand.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ShowAllTeamMembersCommand extends BaseCommand {
 
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 5;
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
 
     public ShowAllTeamMembersCommand(TaskManagementRepository taskManagementRepository) {
         super(taskManagementRepository);
