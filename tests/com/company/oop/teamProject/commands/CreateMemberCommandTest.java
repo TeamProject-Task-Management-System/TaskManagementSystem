568c8e10fc14ee2a463302088ba01e4c0635ecad
Asus <i.t.nachkov@abv.bg>
2024-05-22 23:50:25 +0300

    CreateMemberCommandTest creating

---
+++ tests/com/company/oop/teamProject/commands/CreateMemberCommandTest.java
@@ -0,0 +1,44 @@
+package com.company.oop.teamProject.commands;
+
+import com.company.oop.teamProject.command.CreateMemberCommand;
+import com.company.oop.teamProject.command.CreateTeamCommand;
+import com.company.oop.teamProject.core.TaskManagementRepositoryImpl;
+import com.company.oop.teamProject.core.contracts.TaskManagementRepository;
+import com.company.oop.teamProject.utils.TestUtilities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CreateMemberCommandTest {
+
+    private static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+    private TaskManagementRepository repository;
+    private CreateMemberCommand createMemberCommand;
+
+    @BeforeEach
+    public void before() {
+        repository = new TaskManagementRepositoryImpl();
+        createMemberCommand = new CreateMemberCommand(repository);
+    }
+
+    @Test
+    public void should_ThrowException_When_ArgumentCountDifferentThanExpected() {
+        List<String> params = TestUtilities.getList(EXPECTED_NUMBER_OF_ARGUMENTS - 1);
+
+        assertThrows(IllegalArgumentException.class, () -> createMemberCommand.execute(params));
+    }
+
+    @Test
+    public void execute_Should_AddNewMember_When_PassedValidInput() {
+
+        List<String> params = List.of("Petar");
+
+        createMemberCommand.execute(params);
+
+        Assertions.assertEquals(1, repository.getMembers().size());
+    }
+}
